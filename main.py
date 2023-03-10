6cef91a475cea36faff55b65b8b93abe7ff43ceb
nobio2009 <nobio2009@gmail.com>
2023-03-10 19:23:13 +0100

    third commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pygame 
+
+pygame.init()
+Hight = 600
+Width = 800
+pygame.display.set_mode([Hight, Width])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+pygame.quit()
